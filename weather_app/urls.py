f0832ef56d2d95bc9e9413ac8bdd1e164685d740
niccoboa <nicco.boa@gmail.com>
2023-06-13 21:15:59 +0200

    New static folder

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -5,7 +5,8 @@ from .views import WeatherForecastInfo
 urlpatterns = [
     # API endpoints
     path('forecasts/', WeatherForecastInfo.as_view(), name='forecasts'),
-    path('', views.homepage)
+    path('', views.homepage),
+    path('home/', views.homepage)
 ]
 
 
