77ea5efddaf1add6bf5f5b58232c576f3c4901a6
niccoboa <nicco.boa@gmail.com>
2023-06-13 19:36:42 +0200

    Collectstatic, again

--- WeatherAPI/settings.py
+++ WeatherAPI/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/rest_framework/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
